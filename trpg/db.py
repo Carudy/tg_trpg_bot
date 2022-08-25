118ce5abc40dc2c0d8fce5e57f468a42ea72b827
Vie <sunrise.han@foxmail.com>
2022-08-26 00:31:12 +0800

    memory storage

--- trpg/db.py
+++ trpg/db.py
@@ -1,33 +1,26 @@
-import shelve
-
-DB_FILE = './data'
+db = dict()
 
 
 def reset():
-    with shelve.open(DB_FILE, flag='c') as db:
-        for k in db:
-            del db[k]
-        db['pc'] = {}
-        db['bind'] = {}
-        db['mod'] = {}
-        db['kp'] = None
+    global db
+    db = dict()
 
 
 def set(k, v):
-    with shelve.open(DB_FILE, flag='c') as db:
-        db[k] = v
+    global db
+    db[k] = v
 
 
 def get(k):
-    with shelve.open(DB_FILE, flag='c') as db:
-        if k in db:
+    global db
+    if k in db:
+        return db[k]
+    else:
+        if k in ['pc', 'bind', 'mod']:
+            db[k] = {}
+            return db[k]
+        elif k in ['kp']:
+            db[k] = None
             return db[k]
         else:
-            if k in ['pc', 'bind', 'mod']:
-                db[k] = {}
-                return db[k]
-            elif k in ['kp']:
-                db[k] = None
-                return db[k]
-            else:
-                return None
+            return None
